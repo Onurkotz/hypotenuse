75f446c3b3a99fffc91eeeced5be9f8e786f849e
onur koc <onurkottz@gmail.com>
2022-04-09 12:49:34 +0300

    added some formula

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 #JAVA
 
-Calculate hypotenuse, perimetre and area of a 90° triangle
+Calculate hypotenuse, perimetre and area of a 90° triangle and area of triangle.
--- src/triangle.java
+++ src/triangle.java
@@ -4,13 +4,14 @@ public class triangle {
     public static void main(String[] args) {
 
         double a, b,hypotenuse, area, perimetre;
+        ///////  90° Triangle
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter first side of triangle type in centimetre: ");
+        System.out.print("Enter first side of 90° triangle type in centimetre: ");
         a = input.nextInt();
         System.out.println(a + " cm");
 
-        System.out.print("Enter second side of triangle type in centimetre: ");
+        System.out.print("Enter second side of 90° triangle type in centimetre: ");
         b = input.nextInt();
         System.out.println(b + " cm");
 
@@ -21,7 +22,27 @@ public class triangle {
         System.out.println("Perimetre of triangle: " + perimetre + " cm");
 
         area = (a * b) / 2;
-        System.out.println("Area of triangle: " + area + " square cm");
+        System.out.println("Area of 90° triangle: " + area + " square cm");
 
+
+        /////// Triangle
+
+        double e, f, g, u, ar;
+        System.out.print("Enter first side of triangle type in centimetre: ");
+        e = input.nextInt();
+        System.out.println(a + " cm");
+
+        System.out.print("Enter second side of triangle type in centimetre: ");
+        f = input.nextInt();
+        System.out.println(b + " cm");
+
+
+        System.out.print("Enter third side of triangle type in centimetre: ");
+        g = input.nextInt();
+        System.out.println(b + " cm");
+
+        u = (e + f + g ) / 2;
+        ar = Math.sqrt((u * (u - e)  * (u - f)  * (u - g)));
+        System.out.println("Area of triangle: " + ar + " square cm");
     }
 }
